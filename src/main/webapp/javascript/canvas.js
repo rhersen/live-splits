4b893e4ec6a37c18c9ca8ad76cb0891cc9925e3b
unknown <rudolf@.(none)>
2011-10-26 20:57:14 +0200

    implemented course drawing

--- src/main/webapp/javascript/canvas.js
+++ src/main/webapp/javascript/canvas.js
@@ -1,3 +1,21 @@
+function getCoordinateMapper(domain, valueForMin, valueForMax) {
+    if (!domain.length) {
+        return function (x) {
+            return x;
+        };
+    }
+    var max = Math.max.apply(Math, domain);
+    var min = Math.min.apply(Math, domain);
+    if (min === max) {
+        return function (x) {
+            return x - min + (valueForMin + valueForMax) / 2;
+        };
+    }
+    var scale = (valueForMax - valueForMin) / (max - min);
+    return function (x) {
+       return valueForMin + scale * (x - min);
+    };
+}
 function getCanvas() {
     return $('canvas')[0];
 }
@@ -6,16 +24,26 @@ function getContext() {
     return getCanvas().getContext('2d');
 }
 
-function getMillisSinceUpdate() {
-    return getMillisSinceRefresh(state.currentDate, getMillisFromMidnight(state.updated));
-}
-
-function draw(context, competitor) {
-    context.fillText(competitor.timeString, 120, 120);
+function draw(canvas, competitor) {
+    var context = canvas.getContext('2d');
+    var splits = competitor.splits;
+    var xs = [];
+    var ys = [];
+    var i;
+    for (i = 0; i < splits.length; ++i) {
+        xs.push(splits[i].control.x);
+        ys.push(splits[i].control.y);
+    }
+    var xmapper = getCoordinateMapper(xs, 0, canvas.width - 40);
+    var ymapper = getCoordinateMapper(ys, canvas.height - 20, 20);
+    for (i = 0; i < splits.length; ++i) {
+        context.fillText(splits[i].time, xmapper(splits[i].control.x), ymapper(splits[i].control.y));
+    }
 }
 
 function init(id) {
     var canvas = getCanvas();
+    var competitor;
     run();
     window.onresize = handleResize;
     handleResize();
@@ -25,9 +53,10 @@ function init(id) {
     }
 
     function handleSuccess(x) {
-        draw(getContext(), x);
+        competitor = x;
+        draw(getCanvas(), x);
     }
-    
+
     function handleError(x) {
         alert(x);
     }
@@ -39,5 +68,7 @@ function init(id) {
 
         canvas.width = window.innerWidth - (margin * 2);
         canvas.height = window.innerHeight - (margin * 2);
+
+        draw(getCanvas(), competitor);
     }
 }
--- src/test/javascript/canvas-specs.js
+++ src/test/javascript/canvas-specs.js
@@ -10,7 +10,8 @@ describe('canvas', function() {
         };
     }
 
-    var nop = function () {};
+    var nop = function () {
+    };
 
     function width(value) {
         return function () {
@@ -23,32 +24,95 @@ describe('canvas', function() {
         return matches[1];
     }
 
-    it("draw no departures", function() {
-        var calls = 0;
-        var contextMock = {
-            fillRect: nop,
-            fillText: function () {
-                ++calls;
-            },
-            measureText: width(200)
-        };
-
-        expect(calls).toBe(0);
+    it("should return identity function", function() {
+        var target = getCoordinateMapper([0, 100], 0, 100);
+        expect(target(0)).toBe(0);
+        expect(target(100)).toBe(100);
     });
 
-    it("draw station", function() {
-        var xActual = 0;
-        var yActual = 0;
+    it("should scale", function() {
+        var target = getCoordinateMapper([0, 1000], 0, 100);
+        expect(target(100)).toBe(10);
+    });
+
+    it("should use maximum domain value", function() {
+        var target = getCoordinateMapper([0, 10, 1000], 0, 100);
+        expect(target(100)).toBe(10);
+    });
+
+    it("should translate", function() {
+        var target = getCoordinateMapper([1000, 900], 0, 100);
+        expect(target(900)).toBe(0);
+    });
+
+    it("should scale and translate", function() {
+        var target = getCoordinateMapper([1000, 900], 0, 200);
+        expect(target(900)).toBe(0);
+    });
+
+    it("should map min to valueForMin", function() {
+        var target = getCoordinateMapper([1000, 900], 100, 200);
+        expect(target(900)).toBe(100);
+    });
+
+    it("should flip if when valueForMin is greater than valueForMax", function() {
+        var target = getCoordinateMapper([1000, 900], 200, 100);
+        expect(target(900)).toBe(200);
+        expect(target(1000)).toBe(100);
+    });
+
+    it("should handle only one domain value", function() {
+        var target = getCoordinateMapper([1000], 0, 100);
+        expect(target(1000)).toBe(50);
+        expect(target(999)).toBeLessThan(50);
+    });
+
+    it("should handle empty domain", function() {
+        var target = getCoordinateMapper([], 0, 100);
+        expect(target(1000)).toBe(1000);
+    });
+
+    it("draw wide", function() {
+        var time;
+        var nCalls = 0;
+        var xMin = 10000;
+        var xMax = 0;
+        var yMin = 10000;
+        var yMax = 0;
         var contextMock = {
             fillRect: nop,
             fillText: function (text, x, y) {
-                xActual = x;
-                yActual = y;
+                time = text;
+                ++nCalls;
+                if (x < xMin) {
+                    xMin = x;
+                }
+                if (y < yMin) {
+                    yMin = y;
+                }
+                if (x > xMax) {
+                    xMax = x;
+                }
+                if (y > yMax) {
+                    yMax = y;
+                }
             },
             measureText: width(200)
         };
-        draw(contextMock, {"timeString":"45.40"});
-        expect(xActual > 0).toBeTruthy();
+        draw(createCanvasMock(contextMock), {"name":"Tilda Andersson","id":"23493","time":"20.05","splits":[
+            {"time":"02.40","control":{"x":3386.1,"y":2601.15,"code":"52"}},
+            {"time":"10.35","control":{"x":2682.6,"y":2065.8,"code":"55"}}
+        ]});
+        expect(xMin).not.toBeLessThan(0);
+        expect(xMin).toBeLessThan(120);
+        expect(xMax).toBeGreaterThan(600);
+        expect(xMax).toBeLessThan(800);
+        expect(yMin).toBeGreaterThan(10);
+        expect(yMin).toBeLessThan(320);
+        expect(yMax).toBeGreaterThan(320);
+        expect(yMax).toBeLessThan(640);
+        expect(nCalls).toBe(2);
+        expect(time.charAt(2)).toBe('.');
     });
 
 });
