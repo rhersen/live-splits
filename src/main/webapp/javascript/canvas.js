9feac2e5ebc19462d0c9f5f469edfc8723784390
unknown <rudolf@.(none)>
2011-10-27 21:49:13 +0200

    refactored draw font properties

--- src/main/webapp/javascript/canvas.js
+++ src/main/webapp/javascript/canvas.js
@@ -1,3 +1,22 @@
+function each(a, f) {
+    for (var i = 0; i < a.length; ++i) {
+        f(a[i]);
+    }
+}
+function map(a, f) {
+    var r = [];
+
+    for (var i = 0; i < a.length; ++i) {
+        r.push(f(a[i]));
+    }
+
+    return r;
+}
+
+function getCanvas() {
+    return $('canvas')[0];
+}
+
 function getCoordinateMapper(domain, valueForMin, valueForMax) {
     if (!domain.length) {
         return function (x) {
@@ -16,62 +35,77 @@ function getCoordinateMapper(domain, valueForMin, valueForMax) {
        return valueForMin + scale * (x - min);
     };
 }
-function getCanvas() {
-    return $('canvas')[0];
-}
 
-function getContext() {
-    return getCanvas().getContext('2d');
-}
-
-function controlRing(context, x, y) {
-    context.beginPath();
-    context.arc(x, y, 32, 0, 2 * Math.PI, false);
-    context.fillStyle = "white";
-    context.fill();
-    context.stroke();
-}
-function controlTime(context, text, x, y) {
-    context.fillStyle = "black";
-    context.fillText(text, x, y);
-}
-function controlLines(splits, xmapper, ymapper, c) {
-    var i = 1;
-    var x = xmapper(splits[0].control.x);
-    var y = ymapper(splits[0].control.y);
-    c.beginPath();
-    c.moveTo(x, y);
-    for (i = 1; i < splits.length; ++i) {
-        x = xmapper(splits[i].control.x);
-        y = ymapper(splits[i].control.y);
-        c.lineTo(x, y);
-    }
-    c.lineWidth = 2;
-    c.strokeStyle = "red";
-    c.stroke();
-    return {x:x, y:y, i:i};
-}
 function draw(canvas, competitor) {
     var c = canvas.getContext('2d');
-    var splits = competitor.splits;
-    var xs = [];
-    var ys = [];
-    var i;
-    for (i = 0; i < splits.length; ++i) {
-        xs.push(splits[i].control.x);
-        ys.push(splits[i].control.y);
+    var xmapper = getCoordinateMapper(mapControlX(competitor.splits), 33, canvas.width - 33);
+    var ymapper = getCoordinateMapper(mapControlY(competitor.splits), canvas.height - 33, 33);
+
+    controlLines(c, competitor.splits, xmapper, ymapper);
+
+    each(competitor.splits, function (split) {
+            var center = mapControl(split.control, xmapper, ymapper);
+            controlRing(c, center.x, center.y);
+        }
+    );
+
+    setFont(c);
+
+    each(competitor.splits, function (split) {
+            var center = mapControl(split.control, xmapper, ymapper);
+            controlTime(c, split.time, center.x, center.y);
+        }
+    );
+
+    function mapControlX(splits) {
+        return map(splits, function (split) {
+            return split.control.x;
+        });
     }
-    var xmapper = getCoordinateMapper(xs, 33, canvas.width - 33);
-    var ymapper = getCoordinateMapper(ys, canvas.height - 33, 33);
-    var __ret = controlLines(splits, xmapper, ymapper, c);
-    var x = __ret.x;
-    var y = __ret.y;
-    i = __ret.i;
-    for (i = 0; i < splits.length; ++i) {
-        x = xmapper(splits[i].control.x);
-        y = ymapper(splits[i].control.y);
-        controlRing(c, x, y);
-        controlTime(c, splits[i].time, x, y);
+
+    function mapControlY(splits) {
+        return map(splits, function (split) {
+            return split.control.y;
+        });
+    }
+
+    function setFont(c) {
+        c.font = '24px sans-serif';
+        c.textAlign = 'center';
+        c.textBaseline = 'middle';
+        c.fillStyle = 'black';
+    }
+
+    function controlLines(c, splits, xmapper, ymapper) {
+        var i = 1;
+        var x = xmapper(splits[0].control.x);
+        var y = ymapper(splits[0].control.y);
+        c.beginPath();
+        c.moveTo(x, y);
+        for (i = 1; i < splits.length; ++i) {
+            x = xmapper(splits[i].control.x);
+            y = ymapper(splits[i].control.y);
+            c.lineTo(x, y);
+        }
+        c.lineWidth = 2;
+        c.strokeStyle = "red";
+        c.stroke();
+    }
+
+    function mapControl(control, xmapper, ymapper) {
+        return {x:xmapper(control.x), y:ymapper(control.y)};
+    }
+
+    function controlRing(c, x, y) {
+        c.beginPath();
+        c.arc(x, y, 32, 0, 2 * Math.PI, false);
+        c.fillStyle = "white";
+        c.fill();
+        c.stroke();
+    }
+
+    function controlTime(c, time, x, y) {
+        c.fillText(time, x, y);
     }
 }
 
